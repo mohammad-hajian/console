429e6990424005b4b47dc47800a51beb2b77484f
mohammad-hajian <mohammad.hajian.1375@gmail.com>
2018-07-03 20:35:44 +0430

    dars 3

--- beginner/Program.cs
+++ beginner/Program.cs
@@ -10,6 +10,62 @@ namespace beginner
     {
         static void Main(string[] args)
         {
+            //dars 1 ##############################################################################
+            Console.WriteLine("hello world!");
+            Console.ReadKey();
+            //dars 2 ##############################################################################
+            int a; long b = 966;
+            a = (int)b;
+            //dars 3 ##############################################################################
+            //bool wqw = true;//false
+            //int i = 1, q;
+            //i++;
+            //++i;
+            //q = ++i;
+            //q = i++;
+
+            //while (true)
+            //{
+
+            //}
+
+            //do
+            //{
+
+            //} while (true);
+            
+            //try
+            //{
+
+            //}
+            //catch (Exception p)
+            //{
+            //    Console.WriteLine(p.Message);
+            //}
+
+            //switch (i)
+            //{
+            //    case 1: Console.WriteLine("one");
+            //        break;
+            //    case 2: Console.WriteLine("two");
+            //        break;
+            //    case 3: Console.WriteLine("three");
+            //        break;
+            //    case 4: 
+            //    case 5: 
+            //    case 6: 
+            //    case 7: Console.WriteLine("four");
+            //        break;
+            //    default:
+            //        Console.WriteLine("d");
+            //        break;
+            //}
+
+            //for (int z = 0; z < 12; z++)
+            //{
+            //    i++;
+            //}
+
         }
     }
 }
